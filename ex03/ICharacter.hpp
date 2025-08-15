e36f08427ef126d3868544d1fd47bf5477068bdb
Manuel Mazunga De Almeida <maalmeid@c2r1s9.42luanda.com>
2025-08-15 10:59:46 +0100

    Added ICharacter

---
+++ ex03/ICharacter.hpp
@@ -0,0 +1,22 @@
+#ifndef ICHARACTER_HPP
+# define ICHARACTER_HPP
+
+#include <string>
+
+class AMateria;
+
+class ICharacter
+{
+    public:
+        virtual ~ICharacter();
+
+        virtual std::string const & getName() const = 0;
+
+        virtual void equip(AMateria* m) = 0;
+
+        virtual void unequip(int index) = 0;
+
+        virtual void use(int index, ICharacter& target) = 0;
+};
+
+#endif
